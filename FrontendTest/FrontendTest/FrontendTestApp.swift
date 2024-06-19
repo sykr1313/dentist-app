77e13c5e1c4714cd954dc8747dff268b14343a0f
sykr1313 <167267895+sykr1313@users.noreply.github.com>
2024-06-19 16:54:42 +0900

    Swift 프로젝트 추가

---
+++ FrontendTest/FrontendTest/FrontendTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrontendTestApp.swift
+//  FrontendTest
+//
+//  Created by ksy on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct FrontendTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
